a27639dd7497887e62fdba13a2bd06eade5d9b22
Daniel Sadiq <danielsadiq93@gmail.com>
2024-08-16 15:27:27 +0100

    First react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+let clicks = 0;
 
-function App() {
+
+export default function App() {
+	const [advice, setAdvice] = useState("");
+	const [count, setCount] = useState(0);
+
+	async function getAdvice() {
+	try {
+		const req = await fetch("https://api.kanye.rest/");
+		const data = await req.json();
+		clicks++;
+		setAdvice(data.quote);
+		setCount((c) => c + 1);
+	} catch (error) {
+		console.log("There's an error");
+	}
+	}
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div>
+		<h1>{advice}</h1>
+		<button onClick={getAdvice}>Get Advice</button>
+		<Message count = {count} />
+	</div>
   );
 }
+// A component is basically a function
+// It returns HTML like syntax known as JSX
+// Whenever we need something to change in the UI,
+// we change/update the state
 
-export default App;
+// use effect takes in 2 arguments, a function and an array
+
+function Message(props){
+	return(
+		<p>
+			You have read <strong>{props.count}</strong> pieces of advice
+		</p>
+	)
+};
